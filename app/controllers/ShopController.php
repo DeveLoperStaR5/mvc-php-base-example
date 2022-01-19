f7ab934cf011221c9dbc0b0acfa0ee1fbc3d2974
DeveLoperStaR5 <98062933+DeveLoperStaR5@users.noreply.github.com>
2022-01-20 03:15:24 +0330

    Add files via upload

---
+++ app/controllers/ShopController.php
@@ -0,0 +1,17 @@
+<?php
+
+class ShopController extends ApplicationController
+{
+	public function indexAction()
+	{
+		$this->view->message = "hello from test::index";
+	}
+	
+	public function checkAction()
+	{
+		echo "hello from test::check";
+	}
+}
+
+
+?>
